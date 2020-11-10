bc69e91c01fadc38ad93b9d777e2a8991f91d236
UnaiVadillo <56158238+UnaiVadillo@users.noreply.github.com>
2020-11-10 20:28:17 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+ var numsClave = [0,1,2,3,4,5,6,7,8,9];
+    numsClave = numsClave.sort(function() {return Math.random() - 0.5});
+    console.log(numsClave);
+    
+
+$(document).ready(function(){
+   
+});
