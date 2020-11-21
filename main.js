3cf35b7cb524741cf1d3a7f9c2487128e56a91c9
UnaiVadillo <56158238+UnaiVadillo@users.noreply.github.com>
2020-11-21 16:22:29 +0100

    acabado

--- main.js
+++ main.js
@@ -1,22 +1,29 @@
 var usuarios = [];
 
-    var movimiento1 ={fecha: new Date(2015,10,7),
+var movimiento1 ={fecha: new Date(2015,10,7),
                       concepto:"Compra masiva",
                       importe: 1500, 
                       saldo:10000 };
-    var movimiento2 = {fecha: new Date(2018,8,25),
-                       concepto:"Compra pc",
+var movimiento2 = {fecha: new Date(2018,8,25),
+                       concepto:"Pc nuevo",
                        importe: 1200, 
-                       saldo:2400};
+                       saldo:21400};
+var movimiento3 = {fecha: new Date(2014,4,2),
+                        concepto:"Coche segunda mano",
+                        importe: 2400, 
+                        saldo:32400};
+var movimiento4 = {fecha: new Date(2003,9,21),
+                            concepto:"Casa",
+                            importe: 10200, 
+                            saldo:3210};
 
-    var cuenta1 = {numCuenta:11111111111, movimientos: [movimiento1, movimiento2]};
-    var cuenta2 = {numCuenta:22222222222, movimientos: movimiento2};
+var cuenta1 = {numCuenta:11111111111, movimientos: [movimiento1, movimiento2]};
+var cuenta2 = {numCuenta:22222222222, movimientos: [movimiento3, movimiento4]};
 
-    var perfil1  = {dni: "72849750X", contra:987654, cuenta: [cuenta1, cuenta2]};
-    var perfil2 = {dni: "89854398B", contra:123456};
+var perfil1  = {dni: "72849750X", contra:987654, cuenta: [cuenta1, cuenta2]};
+var perfil2 = {dni: "89854398B", contra:123456, cuenta: [cuenta1]};
 
-    usuarios.push(perfil1,perfil2);
-
+usuarios.push(perfil1,perfil2);
 
 
 function generarBotones(){
@@ -63,37 +70,37 @@ function mostrarCuentas(){
     var splitCookie= document.cookie.split("=");
     var dniCookie= splitCookie[1];
     var dniusu = usuarios.find(user=>dniCookie ===user.dni);
+    console.log(dniusu.cuenta.length);
     for (x=0;x<dniusu.cuenta.length;x++){
 		$("#cuentas").append('<option value="'+x+'">'+dniusu.cuenta[x].numCuenta+'</option>');
     }
 }
 
 function crearTabla(){
+    movimientosCuenta = obtenerArrayMov();
+    rellenarTabla(movimientosCuenta);
+   
+}
+function obtenerArrayMov(){
     var splitCookie= document.cookie.split("=");
     var dniCookie= splitCookie[1];
     var dniusu = usuarios.find(user=>dniCookie ===user.dni);
     var movimientosCuenta= dniusu.cuenta[$('#cuentas').val()].movimientos;
-    
-    if($('#fecha').prop('checked')){
-        movimientosCuenta.sort(function(a, b) { a = new Date(a.fecha); b = new Date(b.fecha); return a>b ? -1 : a<b ? 1 : 0; });
-    }else{
-        movimientosCuenta = movimientosCuenta.sort(function (a, b) {
-            if (a.importe > b.importe) {
-                return 1;
-            }
-            if (a.importe < b.importe) {
-                return -1;
-            }
-            // a must be equal to b
-            return 0;
-        });
-    }
-    
+    return movimientosCuenta;
+}
+function ordenarTablas(){
+    var arrayOrdenado = [];
+    movimientosCuenta = obtenerArrayMov();
+     if($('#fecha').prop('checked')){
+        arrayOrdenado = movimientosCuenta.sort((a, b) => b.fecha - a.fecha);
+     }else{
+        arrayOrdenado = movimientosCuenta.sort((a, b) => b.importe - a.importe);
+     }
+     rellenarTabla(arrayOrdenado);
+}
+function rellenarTabla(movimientosCuenta){
     $('#tablaMovimientos').empty();
     $('#tablaMovimientos').append('<tbody><th>Concepto</th><th>Importe</th><th>Saldo</th><th>Fecha</th></tbody>');
-    
-    console.log()
-    console.log(movimientosCuenta);
     for (x = 0 ; x < movimientosCuenta.length; x++) {
         $('#tablaMovimientos tr:last').after('<tr><td>'+movimientosCuenta[x].concepto+'</td><td>'+movimientosCuenta[x].importe+'</td><td>'+movimientosCuenta[x].saldo+'</td><td>'+movimientosCuenta[x].fecha+'</td></tr>');
     }
